2bbe829fe1c84e46566d9d8483cb02b5e4a78713
zovi <jakub.zovak343@gmail.com>
2019-01-09 13:15:08 +0100

    changed form.test.js

--- src/test/form.test.js
+++ src/test/form.test.js
@@ -1,12 +1,3 @@
-const timeout = process.env.SLOWMO ? 30000 : 10000;
-
-beforeAll(async () => {
-    await page.goto(URL, {waitUntil: 'domcontentloaded'});
-});
-
-describe('Test form', () => {
-    // here goes your code ...
-});
 
 
 
